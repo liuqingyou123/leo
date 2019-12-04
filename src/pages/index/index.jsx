405ad5cadd4c455c595bcfbc8125c7b42c23bf10
canfoo <canfoo@126.com>
2019-12-04 14:21:56 +0800

    style: 调整代码格式

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -3,39 +3,25 @@ import { View, Text } from '@leojs/components'
 import './index.css'
 
 class Index extends Component {
-
   constructor() {
     super()
     this.state = {
       status: 10
     }
   }
-
   config = {
     navigationBarTitleText: '首页'
   }
-
-  componentWillMount () { }
-
   componentDidMount () {
     console.log('加载')
   }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
   handleClick() {
     this.setState({
       status: this.state.status + 1
     })
     console.log('click')
   }
-
   render () {
-    
     return (
       <View className='container'>
         <View className="btn">
